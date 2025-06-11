3075847c742e78962de0190a674e157d8eba2e9b
Qinyum0 <1135710144@qq.com>
2025-06-11 19:00:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@
 
 import numpy as np
 import pandas as pd
-import shap
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegressionCV
